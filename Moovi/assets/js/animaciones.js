828a94d8af62abf3724566c0d11263d83cdcb4eb
fed-ean <nfede41@gmail.com>
2025-08-25 21:13:19 -0300

    Add files via upload
    Subi todo

---
+++ Moovi/assets/js/animaciones.js
@@ -0,0 +1,11 @@
+tsParticles.load("tsparticles", {
+    background: { color: "#0d0d0d" },
+    particles: {
+        color: { value: "#8823C2" },
+        links: { enable: true, color: "#A6317B" },
+        move: { enable: true, speed: 2 },
+        size: { value: 3 },
+        number: { value: 80 }
+    }
+}
+);
