1356c4b240f448f51ad5263cfb2809a40ac08066
ProgrmadorMeh <huguitoezequiel8@gmail.com>
2025-09-03 15:37:33 -0300

    seguridad

---
+++ functions/get.functions.js
@@ -0,0 +1,51 @@
+import { supabase } from '../supabaseClient.js';
+
+const forms = document.getElementsByClassName('form-get');
+
+Array.from(forms).forEach(form => {
+  const mensajeDiv = form.querySelector('.mensaje');
+  const lista = form.querySelector('.lista-datos');
+
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const tabla = e.target.dataset.tabla;
+
+    if (!tabla) {
+      mensajeDiv.textContent = 'Falta el atributo data-tabla en el formulario.';
+      return;
+    }
+
+    try {
+      const { data, error } = await supabase.from(tabla).select('*');
+
+      if (error) {
+        mensajeDiv.textContent = 'Error al obtener datos: ' + error.message;
+        console.error(error);
+        return;
+      }
+
+      if (!data.length) {
+        mensajeDiv.textContent = 'No se encontraron registros en la tabla.';
+        lista.innerHTML = '';
+        return;
+      }
+
+      // Mostrar resultados
+      lista.innerHTML = '';
+
+      data.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = JSON.stringify(item); 
+        lista.appendChild(li);
+        // Agregarle estilo
+      });
+
+      mensajeDiv.textContent = `Se encontraron ${data.length} registros en "${tabla}".`;
+      console.log('Datos obtenidos:', data);
+
+    } catch (err) {
+      mensajeDiv.textContent = 'Error inesperado: ' + err.message;
+    }
+  });
+});
--- seguridad.js
+++ seguridad.js
@@ -4,18 +4,36 @@ export async function verificacion(){
     const { data: { user }, error: authError } = await supabase.auth.getUser();
     if (authError || !user) {
         alert('Debes iniciar sesión para confirmar la compra');
-        return;
-    }
+        return false;
+    } else return true;
 }
-/*
-export async function verificacion(){
+
+export async function administrador() {
     const { data: { user }, error: authError } = await supabase.auth.getUser();
-    //  where typo_usuario = administrador
-    if ((authError || !user)) {
-        alert('Debes ser administrador para realizar esta accion');
-        return;
+
+    if (authError || !user) {
+        alert('Debes iniciar sesión para realizar esta acción');
+        return false;
     }
+
+    const { data, error } = await supabase
+        .from('user_type')
+        .select('tipo')
+        .eq('auth_user_id', user.id)
+        .single();
+
+    if (error || !data) {
+        alert('No se pudo verificar tu rol de usuario');
+        return false;
+    }
+
+    if (data.tipo_usuario !== 'admin') {
+        alert('Debes ser administrador para realizar esta acción');
+        return false;
+    }
+    return true;
 }
+
 
 export const tablas_visibles = [
     tabla = {
@@ -24,4 +42,4 @@ export const tablas_visibles = [
         etc
     }
 ];
-*/
+
